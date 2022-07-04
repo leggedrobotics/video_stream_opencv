bf86620c0b94f0be2b0341648693b40ce8631a77
Philip <parm@ethz.ch>
2022-07-04 19:20:28 +0200

    comment setting of wrong default values

--- src/video_stream.cpp
+++ src/video_stream.cpp
@@ -288,10 +288,10 @@ virtual void subscribe() {
     cap->set(cv::CAP_PROP_FRAME_HEIGHT, latest_config.height);
   }
 
-  cap->set(cv::CAP_PROP_BRIGHTNESS, latest_config.brightness);
-  cap->set(cv::CAP_PROP_CONTRAST, latest_config.contrast);
-  cap->set(cv::CAP_PROP_HUE, latest_config.hue);
-  cap->set(cv::CAP_PROP_SATURATION, latest_config.saturation);
+  // cap->set(cv::CAP_PROP_BRIGHTNESS, latest_config.brightness);
+  // cap->set(cv::CAP_PROP_CONTRAST, latest_config.contrast);
+  // cap->set(cv::CAP_PROP_HUE, latest_config.hue);
+  // cap->set(cv::CAP_PROP_SATURATION, latest_config.saturation);
 
   if (latest_config.auto_exposure) {
     cap->set(cv::CAP_PROP_AUTO_EXPOSURE, 0.75);
